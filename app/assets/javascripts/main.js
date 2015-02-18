0b3a622bd1dda9a6d36460867eecc2ec3493a146
hs <hs88@mail.ru>
2015-02-18 17:12:55 +0300

    catalog menu

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("[data-slide-src]").click(function(){
     var id = $(this).attr('data-slide-src');
     $("[data-slide-dest='"+id+"']").slideToggle();
+    $(this).parent('.catalog_menu_src').toggleClass('catalog_menu_active');
   });
 
 
